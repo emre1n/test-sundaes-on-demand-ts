f58081b42727741d502b5ce38d356c00a7a3d179
emre1n <emre1n@protonmail.com>
2023-04-18 14:24:05 +0300

    add summary form component

--- src/pages/SummaryForm/index.tsx
+++ src/pages/SummaryForm/index.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const SummaryForm = () => {
-  return <div />;
+  const [tcChecked, setTcChecked] = useState(false);
+  const checkboxChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTcChecked(e.target.checked);
+  };
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+    </span>
+  );
+
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={checkboxChangeHandler}
+          label={checkboxLabel}
+        ></Form.Check>
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
 };
 
 export default SummaryForm;
