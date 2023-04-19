8e9d9264e639f9c238b28d464701acdd9517e317
emre1n <emre1n@protonmail.com>
2023-04-19 12:15:25 +0300

    replace fireEvent with userEvent

--- src/pages/test/SummaryForm.test.tsx
+++ src/pages/test/SummaryForm.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import SummaryForm from '../SummaryForm';
+import userEvent from '@testing-library/user-event';
 
 test('Initial conditions', () => {
   render(<SummaryForm />);
@@ -14,16 +15,18 @@ test('Initial conditions', () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test('Checkbox enables button on first click and disables on second click', () => {
+test('Checkbox enables button on first click and disables on second click', async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
   const checkbox = screen.getByRole('checkbox', {
     name: /terms and conditions/i,
   });
   const confirmButton = screen.getByRole('button', { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
