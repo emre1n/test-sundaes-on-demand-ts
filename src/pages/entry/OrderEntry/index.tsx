8c4f47af2af99584f2f866bd294731d6f267c0f6
emre1n <emre1n@protonmail.com>
2023-04-28 13:58:58 +0300

    add disable order button if no scoops ordered code and test

--- src/pages/entry/OrderEntry/index.tsx
+++ src/pages/entry/OrderEntry/index.tsx
@@ -12,12 +12,18 @@ type TProps = {
 const OrderEntry = ({ setOrderPhase }: TProps) => {
   const { totals } = useOrderDetails();
 
+  // disable order button if there aren't any scoops in order
+  const orderDisabled = totals.scoops === 0;
+
   return (
     <div>
+      <h1>Design Your Sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {formatCurrency(totals.scoops + totals.toppings)}</h2>
-      <Button onClick={() => setOrderPhase('review')}>Order Sundae!</Button>
+      <Button disabled={orderDisabled} onClick={() => setOrderPhase('review')}>
+        Order Sundae!
+      </Button>
     </div>
   );
 };
